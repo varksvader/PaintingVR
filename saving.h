8f2500875caa11af16e18b0bcf27f550afbe2eec
Varnika Sinha <varnsinha@gmail.com>
2017-05-11 07:53:24 -0400

    Update saving.h

--- saving.h
+++ saving.h
@@ -1,6 +1,38 @@
-//
-// Created by CammyBoBammy on 5/10/2017.
-//
+/*
+ * Copyright 2017 SDH Inc. All rights reserved.
+ *
+ * The framework of this app is from the github repository android-sdk-googlevr. Code that relates
+ * to setting up the VR environment, initiating sensors on a Daydream ready phone, and enabling the
+ * controller handler events are all from Google Inc. 
+ *
+ * Additional elements added such as extended color palettes, voice command prompts,
+ * different brush types (opacity, depth, size), paint strokes, erasing, saving, menu pop ups, and
+ * head tracking are created by Varnika Sinha, Sarah Dogar, and Cameron Harvey.
+ *
+ * This app is intended to launch for Daydream users and possibly other VR set users as well. The
+ * app Imagine is the sole product of SDH, whose creators are Varnika Sinha, Sarah Dogar, and
+ * Cameron Harvey. Redistribution of this code or alteration of it is prohibited and may only be
+ * done with permission.
+ *
+ * This app is the product of a MAMS group project for the program Apps for Good. To learn more
+ * about MAMS, Massachusetts Academy of Math and Science, please go to
+ *
+ *      http://massacademy.org/
+ *
+ * To learn more about Apps for Good, please go to
+ *
+ *      http://www.appsforgood.org
+ *
+ * The goal of this project is to create a better VR Painter with 360 span for a wider range of
+ * audiences with minimal excess hardware.
+ *
+ * This code has been created in colloboration with Jinchao Yang. For any questions, he can be contacted at:
+ *
+ *      jyang4@wpi.edu
+ *
+ * This code includes no framework from Google Inc., this is the product of only the colloborators and Jinchao Yang.
+ *
+ */
 
 #ifndef CONTROLLERPAINT_SAVING_H
 #define CONTROLLERPAINT_SAVING_H
